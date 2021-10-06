072da860cb9d1c73c6df99c57b5931fb666e562c
srushtiD13 <srushti.dalvi18@vit.edu>
2021-10-06 09:51:33 +0530

    used this()

--- Day2/Person.java
+++ Day2/Person.java
@@ -7,27 +7,29 @@ public class Person {
 	String emailId;
 	long phone;
 	
-	
+	//using this() we can reduce redundant code. super() will call constr of super class
 	public Person(String name, String gender, String emailId, long phone) {
-		super();
-		this.name = name;
-		this.gender = gender;
-		this.emailId = emailId;
+		
+		//this.name = name;
+		//this.gender = gender;
+		//this.emailId = emailId;
+		this(name,gender,emailId);//it will call coonstr having args string,string,string
 		this.phone = phone;
 	}
 
 
 	public Person(String name, String gender) {
-		super();
+		
 		this.name = name;
 		this.gender = gender;
 	}
 
 
 	public Person(String name, String gender, String emailId) {
-		super();
-		this.name = name;
-		this.gender = gender;
+	
+		//this.name = name;
+		//this.gender = gender;
+		this(name,gender);
 		this.emailId = emailId;
 	}
 	
